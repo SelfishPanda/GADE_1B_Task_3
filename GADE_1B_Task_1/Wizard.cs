5835d677fe8d7e4c4eb460f71b7a47c9033a16c8
SelfishPanda <55157024+SelfishPanda@users.noreply.github.com>
2019-10-06 22:12:29 +0200

    Updated wizard
    gave the wizard class an aoe method so that it can attack correctly

--- GADE_1B_Task_1/Wizard.cs
+++ GADE_1B_Task_1/Wizard.cs
@@ -8,6 +8,8 @@ namespace GADE_1B_Task_1
 {
     class Wizard : unit
     {
+
+
         //CLASS CONSTRUCTORS
         public Wizard(string _unitName, int _xPos, int _yPos, int _HP, int _speed, int _attack, int _attackRange, string _team, char _symbol, bool _isAttacking) : base(_unitName, _xPos, _yPos, _HP, _speed, _attack, _attackRange, _team, _symbol, _isAttacking)
         {
@@ -21,6 +23,21 @@ namespace GADE_1B_Task_1
             string output = "";
             output = "Melee Unit (" + team + ")" + "\n" + "Health Points : " + this.HP + "\n" + "X-Position : " + this.xPos + "\n" + "Y-Position :" + this.yPos;
             return output;
+        }
+
+        public void WizardAOE(unit[] units)
+        {
+            for (int i = 0; i < units.Length; i++)
+            {
+                if (units[i].xPos == this.xPos+1 && units[i].yPos == this.yPos+1|| units[i].xPos == this.xPos - 1 && units[i].yPos == this.yPos - 1|| units[i].xPos == this.xPos - 1 && units[i].yPos == this.yPos + 1|| units[i].xPos == this.xPos + 1 && units[i].yPos == this.yPos - 1|| units[i].xPos == this.xPos  && units[i].yPos == this.yPos + 1|| units[i].xPos == this.xPos  && units[i].yPos == this.yPos - 1|| units[i].xPos == this.xPos + 1 && units[i].yPos == this.yPos || units[i].xPos == this.xPos - 1 && units[i].yPos == this.yPos )
+                {
+                    if (units[i].unitName != "Wizard")
+                    {
+                        Combat(units[i]);
+                    }
+                    
+                }
+            }
         }
     }
 }
